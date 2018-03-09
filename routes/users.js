78cd06153617296bc51b6ff5730f8ea9d484e18a
Mark Skerl <mskerl@my.bcit.ca>
2018-03-09 14:40:48 -0800

    bcrypt added

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 
+/** BCRYPT **/
+const bcrypt = require("bcrypt");
+/** BCRYPT **/
+
 var dbURL = 'postgres://cascadeapp:teamcascade@cascade-db.cxnma2xuxlgy.us-west-2.rds.amazonaws.com:5432/cascadeapp'
 const client = new pg.Client(dbURL);
 client.connect();
@@ -11,8 +15,8 @@ router.post('/', function(req, res, next) {
   	var password = req.body.password;
 
   	client.query(
-  	"SELECT * FROM users WHERE email = $1 AND password = $2 AND is_admin = false",
-  	[email, password], function (err, result){
+  	"SELECT * FROM users WHERE email = $1 AND is_admin = false",
+  	[email], function (err, result){
 
   		if (err) {
   			console.log(err);
@@ -20,17 +24,22 @@ router.post('/', function(req, res, next) {
   		}
 
   		if (result.rows.length > 0) {
-  			console.log(result.rows[0]);
-			  res.send({ 
-				'success': true, 
-				'user': result.rows[0].email, 
-				'user_id': result.rows[0].user_id });
+			console.log(result.rows[0]);
+			var isMatch = bcrypt.compareSync(password, result.rows[0].password)
+				if(isMatch){
+					res.send({ 
+						'success': true, 
+						'user': result.rows[0].email, 
+						'user_id': result.rows[0].user_id });
+				} else {
+					console.log('Passwords doesn\'t match');
+					res.send({ 'success': false, 'message': 'Incorrect password' })
+				}
   		} else {
   			res.send({ 'success': false, 'message': 'User not found' });
   		}
   	}
   );
-  //})
 
 });
 
