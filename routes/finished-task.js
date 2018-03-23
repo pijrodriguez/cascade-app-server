816674d9bd022c56a1746180a87eda589ad17c24
pijrodriguez <pijrodriguez@yahoo.com.ph>
2018-03-23 00:20:30 -0700

    made some changes in what the login function returns, added finished-task still needed to be done

---
+++ routes/finished-task.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+
+var dbURL = 'postgres://cascadeapp:teamcascade@cascade-db.cxnma2xuxlgy.us-west-2.rds.amazonaws.com:5432/cascadeapp'
+const client = new pg.Client(dbURL);
+client.connect();
+
+router.post('/', function(req, res, next) {
+    var goalID = req.body.goalID;
+  	var assignedTo = req.body.assignedTo;
+	
+  	client.query(
+      "UPDATE goals SET finished_date = current_timestamp WHERE goal_id = $1 AND assigned_to = $2;",
+  	[goalID, assignedTo], function (err, result){
+
+  		if (err) {
+  			console.log(err);
+  			res.send({ 'success': false, 'message': 'Could not connect to db'})
+  		}
+
+  		if (result.rows.length > 0) {
+			console.log(result.rows);
+			console.log('SUCCESS');
+			
+  			res.send({ 'success': true, 'tasks': result.rows });
+  		} else {
+  			res.send({ 'success': false, 'message': 'No tasks found.' });
+  		}
+  	}
+  );
+  //})
+
+});
+
+module.exports = router;
