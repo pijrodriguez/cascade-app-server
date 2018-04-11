c4d67b5bab496494d345a510006b8fe9d6466d9d
pijrodriguez <pijrodriguez@yahoo.com.ph>
2018-04-10 17:13:39 -0700

    updated function to get tasks so that it doesn't get the cleared ones

--- routes/password.js
+++ routes/password.js
@@ -15,9 +15,7 @@ router.post('/', function(req, res, next) {
     var new_password = req.body.password;
     var userID = req.body.user_id;
     
-	//querry the database
-	//#####DEVELOPERS NOTE###### added bcrypt function based on Amrit's index.js, but unsure of dependencies for regNewPassword and bpass
-	bcrypt.hash(req.body.regNewPassword, 5, function(err, bpass) {
+	bcrypt.hash(new_password, 5, function(err, bpass) {
 		client.query(
 			"UPDATE users SET password = $1 WHERE user_id = $2;",
 			[bpass, userID], function (err, result){
